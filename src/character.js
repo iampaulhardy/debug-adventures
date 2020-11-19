11242127bf0effabb73cc20adae9d5e8ed8c7e8a
Jennifer Cant <jcant6@gmail.com>
2020-11-19 21:55:46 +0000

    debug exercises complete

---
+++ src/character.js
@@ -0,0 +1,31 @@
+/* eslint-disable func-names */
+function Character(config) {
+    this.name = config.name;
+    this.health = config.health;
+    this.maxHealth = config.maxHealth;
+    this.dialogue = config.dialogue;
+}
+
+Character.prototype = {
+    constructor: Character,
+    get isAlive(){
+        return this.health > 0;
+    },
+    _takeDamage: function(damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    },
+    _heal: function(potion) {
+        this.health += potion;
+        if (this.health >= this.maxHealth) {
+            this.health = this.maxHealth;
+        }
+    },
+    speak: function() {
+        return this.dialogue;
+    },
+};
+
+module.exports = Character;
